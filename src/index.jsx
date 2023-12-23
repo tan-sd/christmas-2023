3ad3a1dfac27a44043902f0321eb24fdf22a554e
Sheng <shengdatan@gmail.com>
2023-12-24 02:24:27 +0800

    Add div and styling for audio img

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
+import useAudio from "./useAudio";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Canvas>
-            <Experience />
-        </Canvas>
-    </React.StrictMode>
-);
+function App() {
+    const { startPlaying, pausePlaying, isPlaying } = useAudio();
+
+    return (
+        <React.StrictMode>
+            <Canvas>
+                <Experience />
+            </Canvas>
+            <div
+                className="audio"
+                onClick={() => {
+                    if (isPlaying) {
+                        pausePlaying();
+                    } else {
+                        startPlaying();
+                    }
+                }}
+            >
+                <img
+                    src={
+                        isPlaying === false
+                            ? "./image/audioMute.png"
+                            : "./image/audioPlaying.png"
+                    }
+                />
+            </div>
+        </React.StrictMode>
+    );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(<App />);
